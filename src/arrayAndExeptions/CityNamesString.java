66aebd51bc6778ec9df58e41e12e737f42fb626b
rumeysa <dyalman@na.edu>
2020-03-01 12:02:20 -0500

    my first commit 2.2020

---
+++ src/arrayAndExeptions/CityNamesString.java
@@ -0,0 +1,15 @@
+package arrayAndExeptions;
+
+public class CityNamesString {
+    public static void main(String[] args) {
+        String [] cities={"NEW YORK","SEATTLE","SAN DIEGO","BOULDER","ANN ARBOR"};
+        for (int i=0; i<=4; i++){
+            System.out.println(cities[i]);
+
+        }
+        System.out.println();
+        for (int i=cities.length-1; i>=0; i--){
+            System.out.println(cities[i]);
+    }
+    }
+}
---
+++ src/arrayAndExeptions/OddNUmbersArray.java
@@ -0,0 +1,26 @@
+package arrayAndExeptions;
+
+public class OddNUmbersArray {
+    public static void main(String[] args) {
+        int [] oddNumber={1,2,3,4,5,6,7,8,9,10,11};
+        int sum=0;
+        //to find some of odd numbers
+        for (int i=0; i<oddNumber.length; i++) {
+            if (oddNumber[i] % 2 != 0){
+                sum=sum+oddNumber[i];
+                System.out.println(oddNumber[i]);
+            }
+        }
+        System.out.println("sum of odd numbers"+sum);
+        int counter=0;
+        //to find total number of odd numbers
+        for (int i=0; i<oddNumber.length; i++){
+            if (oddNumber[i]%2!=0){
+                counter++;
+            }
+        }
+        System.out.println("total number of odd numbers:"+counter);
+
+    }
+
+}
