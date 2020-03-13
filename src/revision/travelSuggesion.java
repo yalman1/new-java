885a571d71aebedd5c5d15c33543ded7a6070172
rumeysa <dyalman@na.edu>
2020-03-12 21:52:24 -0400

    my first commit 2.2020

---
+++ src/revision/travelSuggesion.java
@@ -0,0 +1,46 @@
+package revision;
+import java.util.Scanner;
+public class travelSuggesion {
+    public static void main(String[] args) {
+        suggestDestination();
+    }
+
+
+    public static String suggestDestination(){
+        Scanner myScan = new Scanner(System.in);
+        String destination="";
+        System.out.print("Sea or Mountain? :");
+        String answer=myScan.nextLine();
+        if(answer.equalsIgnoreCase("sea")){
+            System.out.print("East or West? :");
+            answer=myScan.nextLine();
+            if(answer.equalsIgnoreCase("east")){
+                destination="Florida";
+            }
+            else if(answer.equalsIgnoreCase("west")){
+                destination="California";
+            }
+            else{
+                System.out.println("Wrong input");
+            }
+        }
+        else if(answer.equalsIgnoreCase("mountain")){
+            System.out.print("Midland or North? :");
+            answer=myScan.nextLine();
+            if(answer.equalsIgnoreCase("midland")){
+                destination="Kansas";
+            }
+            else if(answer.equalsIgnoreCase("north")){
+                destination="Vermont";
+            }
+            else{
+                System.out.println("Wrong input");
+            }
+        }
+        else {
+            System.out.println("Wrong input");
+        }
+        return destination;
+    }
+
+}
